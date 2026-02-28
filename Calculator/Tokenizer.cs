38a81574348302b5de23ac94fc16f1818ca0ec34
1butenko <ihor.butenko@icloud.com>
2026-02-28 16:10:54 +0200

    Switched return type of tokenize method of tokenizer

--- Calculator/Tokenizer.cs
+++ Calculator/Tokenizer.cs
@@ -1,56 +1,33 @@
 public class Tokenizer
 {
     private char[] operators { get; set; }
-    private ArrayList<string> tokens = new();
-    private string buffer = String.Empty;
+    private ArrayList<char> tokens = new();
+
     public Tokenizer(char[] operators)
     {
         this.operators = operators;
     }
 
-    public ArrayList<string> Tokenize(string input)
+    public ArrayList<char> Tokenize(string input)
     {
         char[] r = input.ToCharArray();
-
         foreach (var s in r)
         {
-            if (char.IsDigit(s))
+            if (char.IsDigit(s) || operators.Contains(s))
             {
-                buffer += s;
+                tokens.Add(s);
             }
-            else if (char.IsWhiteSpace(s))
-            {
-                if (!string.IsNullOrEmpty(buffer))
-                {
-                    tokens.Add(buffer);
-                    buffer = String.Empty;
-                }
-            }
-            else if (operators.Contains(s))
-            {
-                if (!string.IsNullOrEmpty(buffer))
-                {
-                    tokens.Add(buffer);
-                    buffer = String.Empty;
-                }
-                tokens.Add(s.ToString());
-            }
-            else
+            else if (!char.IsWhiteSpace(s))
             {
                 throw new ArgumentException("This symbol is not supported!");
             }
         }
-
-        if (!string.IsNullOrEmpty(buffer))
-            tokens.Add(buffer);
-
         return tokens;
     }
 
     public void GetTokens()
     {
         Console.WriteLine($"Total tokens: {tokens.Count()}\nTokens:");
-
         for (int i = 0; i < tokens.Count(); i++)
         {
             Console.Write(tokens.Get(i));
