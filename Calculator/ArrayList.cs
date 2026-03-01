98706df74b0fb3d3b0e2b83d2a938337a156e3ff
1butenko <ihor.butenko@icloud.com>
2026-03-01 21:02:01 +0200

    Fixed incrementation of pointer

--- Calculator/ArrayList.cs
+++ Calculator/ArrayList.cs
@@ -10,7 +10,7 @@ public class ArrayList<T>
     public void Add(T element)
     {
         array[pointer] = element;
-        pointer += 1;
+        pointer++;
         
         if (pointer == array.Length)
         {
