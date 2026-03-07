5ecfe1e73abcaa1644a4421f7fd995e213444cb1
1butenko <ihor.butenko@icloud.com>
2026-03-07 10:48:01 +0200

    Changed program algorith call via Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,13 +1,13 @@
 ﻿string input = Console.ReadLine() ?? string.Empty;
-char[] r = input.ToCharArray();
 
-char[] operators = { '(', ')', '+', '-', '*', '/' };
+char[] operators = { '(', ')', '+', '-', '*', '/', '^' };
 
 Tokenizer tokenizer = new Tokenizer(operators);
-ArrayList<char> tokens = tokenizer.Tokenize(input);
 Queue<char> output = new();
 Stack<char> stack = new();
 
-Algorithm algorithm = new Algorithm(tokens, output, stack, operators);
+Algorithm algorithm = new Algorithm(output, stack, operators);
 
-Console.WriteLine(string.Join(" ", algorithm.ShuntigYard().ToArray()));
+Calculator calculator = new Calculator(operators, tokenizer, algorithm);
+
+Console.WriteLine(calculator.calculate(input));
