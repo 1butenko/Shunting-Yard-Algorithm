ac6c63d9be2b0d90c34c84d6e70b83a688f1d18d
1butenko <ihor.butenko@icloud.com>
2026-03-09 22:09:22 +0200

    Implemented calculator with support of operation&function calculation

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -2,8 +2,9 @@ public class Calculator
 {
     private Algorithm algorithm;
     private Tokenizer tokenizer;
-    public char[] Symbols {get => operators;}
-    private char[] operators = { '+', '-', '*', '/', '^', 's', 'c', 'm' };
+    private Stack<int> s = new();
+    public char[] Symbols { get => operators; }
+    private char[] operators = { '+', '-', '*', '/', '^' };
 
     public Calculator(Tokenizer tokenizer, Algorithm algorithm)
     {
@@ -15,5 +16,44 @@ public class Calculator
     {
         ArrayList<char> tokens = tokenizer.Tokenize(input);
         char[] rpn = algorithm.ShuntingYard(tokens).ToArray();
+
+        foreach (var t in rpn)
+        {
+            if (char.IsDigit(t))
+            {
+                s.Push(t - '0');
+            }
+            else if (operators.Contains(t) || t == 'm')
+            {
+                int firstNumber = s.Pop();
+                int secondNumber = s.Pop();
+
+                int result = t switch
+                {
+                    '+' => secondNumber + firstNumber,
+                    '-' => secondNumber - firstNumber,
+                    '*' => secondNumber * firstNumber,
+                    '/' => secondNumber / firstNumber,
+                    '^' => (int)Math.Pow(secondNumber, firstNumber),
+                    'm' => Math.Max(secondNumber, firstNumber),
+                    _ => throw new Exception("Operation not found!")
+                };
+
+                s.Push(result);
+            }
+            else if (t == 's' || t == 'c')
+            {
+                int num = s.Pop();
+                int result = t switch
+                {
+                    's' => (int)Math.Sin(Convert.ToDouble(num)),
+                    'c' => (int)Math.Cos(Convert.ToDouble(num)),
+                    _ => throw new Exception("Function not found!")
+                };
+                s.Push(result);
+            }
+        }
+
+        return s.Pop();
     }
 }
