5441f261871c1dfa4b2c1153f56497094a7cfad6
1butenko <ihor.butenko@icloud.com>
2026-03-12 18:38:01 +0200

    Implemented variable data structures

--- Calculator/Structures/ArrayList.cs
+++ Calculator/Structures/ArrayList.cs
@@ -11,17 +11,53 @@ public class ArrayList<T>
     {
         array[pointer] = element;
         pointer++;
-        
+
         if (pointer == array.Length)
         {
             T[] extendedArray = new T[array.Length * 2];
-            
+
             for (var i = 0; i < array.Length; i++)
             {
                 extendedArray[i] = array[i];
             }
 
             array = extendedArray;
+        }
+    }
+
+    public int IndexOf(T element)
+    {
+        for (var i = 0; i < array.Length; i++)
+        {
+            if (Object.Equals(array[i], element))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public bool Contains(T element)
+    {
+        return IndexOf(element) != -1;
+    }
+
+    public void Remove(T element)
+    {
+        for (var i = 0; i < pointer; i++)
+        {
+            if (Object.Equals(array[i], element))
+            {
+                for (var j = i; j < pointer - 1; j++)
+                {
+                    array[j] = array[j + 1];
+                }
+
+                pointer -= 1;
+                return;
+            }
         }
     }
 }
