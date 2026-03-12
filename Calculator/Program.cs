00e1d0d1138989d85038c279c15464b902371c52
1butenko <ihor.butenko@icloud.com>
2026-03-12 18:39:40 +0200

    Updated for variable approach

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,22 +1,39 @@
-﻿string input = Console.ReadLine() ?? string.Empty;
-
-Tokenizer tokenizer = new Tokenizer();
+﻿Tokenizer tokenizer = new Tokenizer();
 Algorithm algorithm = new Algorithm();
+VariableStorage storage = new VariableStorage();
+Calculator calculator = new Calculator(tokenizer, algorithm, storage);
 
-ArrayList<char> tokens = tokenizer.Tokenize(input);
+while (true)
+{
+    string input = Console.ReadLine() ?? string.Empty;
 
-Calculator calculator = new Calculator(tokenizer, algorithm);
+    if (input != string.Empty)
+    {
+        ArrayList<char> tokens = tokenizer.Tokenize(input);
+        char[] rpn = algorithm.ShuntingYard(tokens).ToArray();
 
-char[] rpn = algorithm.ShuntingYard(tokens).ToArray();
+        if (storage.ContainName(input[0]))
+        {
+            Variable? variable = storage.FindByName(input[0]);
+            if (variable != null)
+                Console.WriteLine($"> {variable.Result}");
+        } else if (tokens.Count() == 1 && char.IsLetter(input[0]) && !storage.ContainName(input[0]))
+        {
+            throw new Exception("Variable not found!");
+        }
+        else
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine($"Reverse Polish Notation: {string.Join("", rpn)}");
+            Console.WriteLine("================================================");
+            Console.WriteLine($"Result: {calculator.calculate(input)}");
+            Console.WriteLine("================================================");
+            Console.WriteLine("Visualized AST: ");
 
-Console.WriteLine("================================================");
-Console.WriteLine($"Reverse Polish Notation: {string.Join("", rpn)}");
-Console.WriteLine("================================================");
-Console.WriteLine($"Result: {calculator.calculate(input)}");
-Console.WriteLine("================================================");
-Console.WriteLine("Visualized AST: ");
+            AbstractSyntaxTree ast = new();
 
-AbstractSyntaxTree ast = new();
-
-ast.Build(algorithm.ShuntingYard(tokens).ToArray());
-ast.Visualize();
+            ast.Build(algorithm.ShuntingYard(tokens).ToArray());
+            ast.Visualize();
+        }
+    }
+}
