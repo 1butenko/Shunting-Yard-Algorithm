a54c7a4f715dbbe6d24071180e6cf759dbd88e12
1butenko <ihor.butenko@icloud.com>
2026-03-13 09:23:27 +0200

    Logger for logging calculator ui

---
+++ Calculator/Logger.cs
@@ -0,0 +1,24 @@
+public class Logger()
+{
+    public void Log(AbstractSyntaxTree? ast = null, char[]? rpn = null, int? result = null, bool logAst = false)
+    {
+        if (rpn != null)
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine($"Reverse Polish Notation: {string.Join("", rpn)}");
+        }
+
+        if (result != null)
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine($"Result: {result}");
+        }
+
+        if (logAst && ast != null)
+        {
+            Console.WriteLine("================================================");
+            Console.WriteLine("Visualized AST: ");
+            ast.Visualize();
+        }
+    }
+}
