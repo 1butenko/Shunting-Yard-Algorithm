66a1d475019625a1be9c3873aeaa5cee83f3dd97
1butenko <ihor.butenko@icloud.com>
2026-03-13 09:24:40 +0200

    Added logger, abstrax syntax tree verification, fixed logger output

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,39 +1,24 @@
-﻿Tokenizer tokenizer = new Tokenizer();
+﻿Logger logger = new Logger();
+Tokenizer tokenizer = new Tokenizer();
 Algorithm algorithm = new Algorithm();
 VariableStorage storage = new VariableStorage();
-Calculator calculator = new Calculator(tokenizer, algorithm, storage);
+Calculator calculator = new Calculator(algorithm, logger);
+VariableManager variableManager = new VariableManager(storage, calculator);
 
-while (true)
-{
+while (true){
     string input = Console.ReadLine() ?? string.Empty;
+    ArrayList<char> tokens = tokenizer.Tokenize(input);
 
-    if (input != string.Empty)
+    int result = variableManager.ProcessVariablesAndCalculate(tokens);
+
+    if (tokens.Count() > 1 && char.IsLetter(tokens.Get(0)) || tokens.Count() == 1 && char.IsLetter(tokens.Get(0)))
     {
-        ArrayList<char> tokens = tokenizer.Tokenize(input);
-        char[] rpn = algorithm.ShuntingYard(tokens).ToArray();
-
-        if (storage.ContainName(input[0]))
-        {
-            Variable? variable = storage.FindByName(input[0]);
-            if (variable != null)
-                Console.WriteLine($"> {variable.Result}");
-        } else if (tokens.Count() == 1 && char.IsLetter(input[0]) && !storage.ContainName(input[0]))
-        {
-            throw new Exception("Variable not found!");
-        }
-        else
-        {
-            Console.WriteLine("================================================");
-            Console.WriteLine($"Reverse Polish Notation: {string.Join("", rpn)}");
-            Console.WriteLine("================================================");
-            Console.WriteLine($"Result: {calculator.calculate(input)}");
-            Console.WriteLine("================================================");
-            Console.WriteLine("Visualized AST: ");
-
-            AbstractSyntaxTree ast = new();
-
-            ast.Build(algorithm.ShuntingYard(tokens).ToArray());
-            ast.Visualize();
-        }
+        logger.Log(result: result);
+    }
+    else
+    {
+        AbstractSyntaxTree ast = new();
+        ast.Build(algorithm.ShuntingYard(tokens).ToArray());
+        logger.Log(ast: ast, result: result, logAst: true);
     }
 }
